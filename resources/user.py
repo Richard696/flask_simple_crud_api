84a3ca8f45b04520d8a3ff81991770549ad9fd44
YeeHeng <lauyh87@gmail.com>
2021-04-11 14:40:57 +0800

    add try catch for api

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,9 @@
 from flask import jsonify, request, Response
 from flask_restful import Resource
-from database.model import User
+from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist, ValidationError, InvalidQueryError
 from flask_bcrypt import generate_password_hash
+from database.model import User
+from resources.error import SchemaValidationError, UserNotExistsError, UserAlreadyExistsError, UpdatingUserError, InternalServerError, DeletingUserError
 
 
 class UsersApi(Resource):
@@ -14,29 +16,55 @@ class UsersApi(Resource):
     # more info regarding bcrypt package -> https://pypi.org/project/bcrypt/
 
     def post(self):
-        body = request.get_json()
-        print("Content of the request:")
-        print(body)
-        password_hash = generate_password_hash(body.get('password'))
-        body['password'] = password_hash
-        user = User(**body).save()
-        id = user.id
-        return {'msg': 'user is created successfully', 'id': str(id)}, 200
+        try:
+            body = request.get_json()
+            print("Content of the request:")
+            print(body)
+            password_hash = generate_password_hash(body.get('password'))
+            body['password'] = password_hash
+            user = User(**body).save()
+            id = user.id
+            return {'msg': 'user is created successfully', 'id': str(id)}, 200
+        except (FieldDoesNotExist, ValidationError):
+            raise SchemaValidationError
+        except NotUniqueError:
+            raise UserAlreadyExistsError
+        except Exception as err:
+            raise InternalServerError
 
 
 class UserAPI(Resource):
+    def get(self, id):
+        try:
+            user = User.objects(id=id).to_json()
+            return Response(user, mimetype="application/json", status=200)
+        except DoesNotExist:
+            raise UserNotExistsError
+        except Exception as err:
+            raise InternalServerError
+
     def put(self, id):
-        body = request.get_json()
-        User.objects.get(id=id).update(**body)
-        return jsonify({
-            "status": 200,
-            "message": "Successfully update the user"
-        })
+        try:
+            body = request.get_json()
+            User.objects.get(id=id).update(**body)
+            return jsonify({
+                "status": 200,
+                "message": "Successfully update the user"
+            })
+        except DoesNotExist:
+            raise UpdatingUserError
+        except Exception as err:
+            raise InternalServerError
 
     def delete(self, id):
-        body = request.get_json()
-        User.objects.get(id=id).delete()
-        return jsonify({
-            "status": 200,
-            "message": "Successfully delete the user"
-        })
+        try:
+            user = User.objects.get(id=id)
+            user.delete()
+            return jsonify({
+                "status": 200,
+                "message": "Successfully delete the user"
+            })
+        except DoesNotExist:
+            raise DeletingUserError
+        except Exception as err:
+            raise InternalServerError
