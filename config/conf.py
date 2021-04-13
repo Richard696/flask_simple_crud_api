2dbf7752256861f0c7fb8e9b51df336ea5459384
YeeHeng <lauyh87@gmail.com>
2021-04-13 22:59:51 +0800

    replace the uri with the genral mongodb uri

--- config/conf.py
+++ config/conf.py
@@ -1,1 +1,6 @@
-dbURI = 'mysql://root:root@db/main'
+dbURI = 'mongodb+srv://<username>:<password>@cluster0.0em0x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+# when finalize replace tje uri with
+# mongodb+srv://<username>:<password>@main-uat.6rr6v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+# in readme add
+# Replace <password> with the password for the developer user. Replace myFirstDatabase with the name of the database that connections will use by default.
