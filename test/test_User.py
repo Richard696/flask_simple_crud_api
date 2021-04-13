0886b3e961f8fded58284b5b80dbc5136fe67bd8
YeeHeng <lauyh87@gmail.com>
2021-04-13 23:00:03 +0800

    add test case for delete user

--- test/test_User.py
+++ test/test_User.py
@@ -76,3 +76,25 @@ class UserTest(BaseCase):
         response = self.app.put(
             "/api/v1/user/" + editedId, headers={"Content-Type": "application/json"}, data=payload)
         self.assertEqual(200, response.status_code)
+
+    def testSuccessfulDeleteUser(self):
+        email = "eugene@qwerty.io"
+        password = "s!mplePassw0rd"
+        role = 1
+        payload = json.dumps({
+            "username": email,
+            "password": password,
+            "role": role
+        })
+
+        response = self.app.post(
+            "/api/v1/user", headers={"Content-Type": "application/json"}, data=payload)
+
+        self.assertEqual(str, type(response.json['id']))
+        self.assertEqual(200, response.status_code)
+
+        editedId = response.json['id']
+
+        response = self.app.delete(
+            "/api/v1/user/" + editedId)
+        self.assertEqual(200, response.status_code)
